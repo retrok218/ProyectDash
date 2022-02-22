5edc47a2656cf1e08079879f9125c853a5f68d3d
retrok218 <retrok218@gmail.com>
2022-02-22 11:24:31 -0600

    ( recarga(on))

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <meta name="description" content="Updates and statistics">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    <meta http-equiv="refresh" content="100">
+    <meta http-equiv="refresh" content="70">
     
    
 
